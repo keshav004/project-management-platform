ce490462e9df067c82e53ff8e4f3c51c6f5b87fd
Keshav Raj <keshavraj004@gmail.com>
2026-07-21 02:56:27 +0530

    add authentication layout

--- src/frontend/project-management-ui/src/app/app.routes.ts
+++ src/frontend/project-management-ui/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard')
+            .then(m => m.Dashboard)
+      }
+    ]
+  }
+];
